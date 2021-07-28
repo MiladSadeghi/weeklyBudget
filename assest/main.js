cc62b10773c74d09fb6b17a0d0dceefbbd13b9cf
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-07-28 14:57:13 +0430

    if nothing enter in input, you see error

--- assest/main.js
+++ assest/main.js
@@ -4,6 +4,22 @@ class HTMLUI {
     budgetTotal.innerHTML = budget
     budgetLeft.innerHTML = budget
   }
+
+  showError(msg) {
+    const div = document.createElement('div')
+    div.classList.add('error')
+    div.appendChild(document.createTextNode(msg))
+
+    const input = document.querySelector('#forError')
+
+    input.insertBefore(div, form)
+
+    setTimeout(function() {
+      div.remove()
+    }, 3000);
+
+    form.reset()
+  }
 }
 
 
@@ -12,6 +28,7 @@ let userBudget;
 const html = new HTMLUI()
 let budgetTotal = document.querySelector('span#total')
 let budgetLeft = document.querySelector('span#residual')
+const form = document.querySelector('#add-expense')
 
 
 // eventListeners
@@ -25,5 +42,15 @@ function eventListeners() {
     } else {
       html.insertBudget(userBudget)
     }
+    form.addEventListener('submit', function(e) {
+      e.preventDefault()
+
+      const expense = document.querySelector('#expense').value
+      const amount = document.querySelector('#amount').value
+
+      if(expense == '' || amount == '') {
+        html.showError('Please Complete All Fields')
+      }
+    })
   })
 }
